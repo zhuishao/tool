274b0d0c2f957b76a22c9812852d8cf181f23e28
朱哲伟 <zhewei.zhu@ecarx.com.cn>
2021-06-15 09:55:03 +0800

    fix(fts-bi)这是一个google扩展插件，可以添加css和js在一个网站中

---
+++ fts-bi/fts-bi/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: ningbo.kang
+ * @Date: 2020-09-27 11:12:19
+ * @LastEditors: ningbo.kang
+ * @LastEditTime: 2020-10-26 09:59:55
+ * @Description: 描述 加载本地样式，更改掘金页面
+ */
+
+window.addEventListener('load', function () {
+  console.log('fts-bi',window.screen.width);
+  // var head = document.getElementsByTagName('head')[0];
+  // var linkTag = document.createElement('link');
+  // linkTag.href = "index.css";
+  // linkTag.setAttribute('rel', 'stylesheet');
+  // linkTag.setAttribute('type', 'text/css');
+  // head.appendChild(linkTag);
+});
